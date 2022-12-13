b7c423045017b7ed34adeb26f1a843619187e037
topisteronyango <t.onyango@alustudent.com>
2022-12-13 14:02:07 +0200

    summative

---
+++ inzu/listings/choices.py
@@ -0,0 +1,51 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+price_choices = {
+  '100000':'$100,000',
+  '200000':'$200,000',
+  '300000':'$300,000',
+  '400000':'$400,000',
+  '500000':'$500,000',
+  '600000':'$600,000',
+  '700000':'$700,000',
+  '800000':'$800,000',
+  '900000':'$900,000',
+  '1000000':'$1M+',
+}
+
+state_choices = {
+        
+       'ALU': 'African Leadership University',
+       'A' : 'Azam' ,
+       'DT' : 'Down Town',
+       'K':'Kicyukiro',
+       'KI':'Kisementi',
+       'P':'Palace',
+       'KM':'Kimironko',
+       'KH':'Kimuhurura',
+       'R': 'Remera',
+       'G':'Geralds',
+       'M': 'Mostar',
+       'KW': 'Kwamushimire',
+       'NY'  : 'Nyamirambo',
+       'ZN': 'Zindiro',
+       'KB': 'Kibagabaga',
+       'CT': 'Contro Technique',
+       'NS' : 'Nyambisindu',
+       'KG' : 'Kigali Heights',
+       'MI' :  'Mirror Hotel',
+       'P':'PolyFarm',
+       'KA' :  'Kigali Arena'
+       
+}
